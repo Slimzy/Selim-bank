7e59b59d01f049d933d29f4921125d00b517ba32
Slimzy <selimbolaji@gmail.com>
2015-11-03 01:45:42 +0100

    Create contentad.js

---
+++ contentad.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+    (function(d) {
+        var params =
+        {
+            id: "fa8894e9-0913-4072-a614-fc236dff80a9",
+            d:  "dG9wd2F5c25nLmNvbQ==",
+            wid: "96611",
+            cb: (new Date()).getTime()
+        };
+
+        var qs=[];
+        for(var key in params) qs.push(key+'='+encodeURIComponent(params
+
+[key]));
+        var s = d.createElement
+
+('script');s.type='text/javascript';s.async=true;
+        var p = 'https:' == document.location.protocol ? 'https' : 'http';
+        s.src = p + "://api.content.ad/Scripts/widget2.aspx?" + qs.join('&');
+        d.getElementById("contentad96611").appendChild(s);
+    })(document);
+</script>
